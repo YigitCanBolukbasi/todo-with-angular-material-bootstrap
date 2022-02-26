4a74fe1ee6cb03e4a3770e04b82c6a676c759fd1
Yigit Can <y.can.bolukbasi@gmail.com>
2022-02-26 11:22:27 +0300

    local storage version finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    DragDropModule,
+    FormsModule,
+    CommonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  text!: string;
+  data: any = {
+    pending: ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'],
+    inProgress: [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog',
+    ],
+    done: ['Get up', 'Brush teeth', 'Take a shower'],
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.setItems();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      Object.keys(this.data).forEach((key) => {
+        localStorage.setItem(key, JSON.stringify(this.data[key]));
+      });
+    }
+  }
+
+  addToDo(todo: string) {
+    this.data.pending.push(todo);
+    this.text = ' ';
+    localStorage.setItem('pendings', JSON.stringify(this.data.pendings));
+  }
+
+  setItems() {
+    Object.keys(this.data).forEach((key) => {
+      if (!localStorage.getItem(key)) {
+        localStorage.setItem(key, JSON.stringify(this.data[key]));
+      } else {
+        this.data[key] = JSON.parse(localStorage.getItem(key) || '{}');
+      }
+    });
+  }
+}
